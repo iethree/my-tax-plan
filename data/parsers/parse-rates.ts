a60cbe213dd819877ce296fe1abf485d82e5ed81
Ryan Laurie <iethree@gmail.com>
2022-03-04 15:58:56 -0600

    reformat parser

--- data/parsers/parse-rates.ts
+++ data/parsers/parse-rates.ts
@@ -1,94 +1,62 @@
-/* eslint-disable */
+/* eslint-disable prettier/prettier */
 
-import papa from "papaparse";
-import fs from "fs";
-import { TaxRates, TaxRate, TaxScheme } from "@/types/taxTypes";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import papa from 'papaparse';
+import fs from 'fs';
+import { TaxRates, TaxRate, TaxScheme } from '@/types/taxTypes';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const rateCSV: any = fs.readFileSync(
-  __dirname + "/../raw/income_tax_rates.csv",
-  "utf8"
-);
-const gainsRateCSV: any = fs.readFileSync(
-  __dirname + "/../raw/gains_tax_rates.csv",
-  "utf8"
-);
-const payrollRateCSV: any = fs.readFileSync(
-  __dirname + "/../raw/payroll_tax_rates.csv",
-  "utf8"
-);
+const rateCSV: any = fs.readFileSync(__dirname + '/../raw/income_tax_rates.csv', 'utf8');
+const gainsRateCSV: any = fs.readFileSync(__dirname + '/../raw/gains_tax_rates.csv', 'utf8');
+const payrollRateCSV: any = fs.readFileSync(__dirname + '/../raw/payroll_tax_rates.csv', 'utf8');
 
 // @ts-ignore
-const rateJSON: any = papa.parse(rateCSV, {
-  header: true,
-  ignoreBlankLines: true,
-  dynamicTyping: true,
-}).data;
+const rateJSON: any = papa.parse(rateCSV, { header: true, ignoreBlankLines: true, dynamicTyping: true }).data;
 // @ts-ignore
-const gainsRateJSON: any = papa.parse(gainsRateCSV, {
-  header: true,
-  ignoreBlankLines: true,
-  dynamicTyping: true,
-}).data;
+const gainsRateJSON: any = papa.parse(gainsRateCSV, { header: true, ignoreBlankLines: true, dynamicTyping: true }).data;
 // @ts-ignore
-const payrollRateJSON: any = papa.parse(payrollRateCSV, {
-  header: true,
-  ignoreBlankLines: true,
-  dynamicTyping: true,
-}).data;
+const payrollRateJSON: any = papa.parse(payrollRateCSV, { header: true, ignoreBlankLines: true, dynamicTyping: true }).data;
 
 // group by status
-const incomeRatesByStatus: TaxRates = rateJSON.reduce(
-  (acc: any, curr: any) => {
-    acc[curr.filingStatus] &&
-      acc[curr.filingStatus].push({
-        rate: curr.rate,
-        min: curr.min,
-        max: curr.max,
-      });
+const incomeRatesByStatus: TaxRates = rateJSON.reduce((acc: any, curr: any) => {
+  acc[curr.filingStatus] && acc[curr.filingStatus].push({
+    rate: curr.rate,
+    min: curr.min,
+    max: curr.max,
+  });
 
-    return acc;
-  },
-  {
-    single: [],
-    marriedFilingJointly: [],
-    marriedFilingSeparately: [],
-    headOfHousehold: [],
-  }
-);
+  return acc;
+}, {
+  single: [],
+  marriedFilingJointly: [],
+  marriedFilingSeparately: [],
+  headOfHousehold: [],
+});
 
-const gainsRatesByStatus: TaxRates = gainsRateJSON.reduce(
-  (acc: any, curr: any) => {
-    acc[curr.filingStatus] &&
-      acc[curr.filingStatus].push({
-        rate: curr.rate,
-        min: curr.min,
-        max: curr.max,
-      });
+const gainsRatesByStatus: TaxRates = gainsRateJSON.reduce((acc: any, curr: any) => {
+  acc[curr.filingStatus] && acc[curr.filingStatus].push({
+    rate: curr.rate,
+    min: curr.min,
+    max: curr.max,
+  });
 
-    return acc;
-  },
-  {
-    single: [],
-    marriedFilingJointly: [],
-    marriedFilingSeparately: [],
-    headOfHousehold: [],
-  }
-);
+  return acc;
+}, {
+  single: [],
+  marriedFilingJointly: [],
+  marriedFilingSeparately: [],
+  headOfHousehold: [],
+});
 
-const payrollRates = payrollRateJSON.reduce(
-  (acc: any, curr: any) => {
-    acc[curr.type].push({
-      rate: curr.rate,
-      min: curr.min,
-      max: curr.max,
-    });
-    return acc;
-  },
-  { socialSecurity: [], medicare: [] }
-);
+const payrollRates = payrollRateJSON.reduce((acc: any, curr: any) =>{
+  acc[curr.type].push({
+    rate: curr.rate,
+    min: curr.min,
+    max: curr.max,
+  });
+  return acc;
+}, { socialSecurity: [], medicare: [] });
 
 const allRates: TaxScheme = {
   income: incomeRatesByStatus,
@@ -96,7 +64,7 @@ const allRates: TaxScheme = {
   payroll: payrollRates,
 };
 
-const fileName = __dirname + "/../rates.json";
+const fileName = __dirname + '/../rates.json';
 
 fs.writeFileSync(fileName, JSON.stringify(allRates, null, 2));
 console.log(`✅ wrote: ${fileName}`);
