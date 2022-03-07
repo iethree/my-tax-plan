3e0492a003d5fc11b2bbf6999921778fc3d4fb11
Ryan Laurie <iethree@gmail.com>
2022-03-07 11:27:41 -0600

    more methodological drivel

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="border-t mx-5 border-opacity-20 p-3 mt-2 text-sm">
-      <a href="https://github.com/iethree/my-tax-plan">
-        <i className="fab fa-github mr-2" />
-        MyTaxPlan is open source
-      </a>
+    <footer className="border-t mx-5 border-opacity-20 p-3 mt-2 text-sm flex justify-between">
+      <div>
+        <a href="https://github.com/iethree/my-tax-plan">
+          <i className="fab fa-github mr-2" />
+          MyTaxPlan is open source
+        </a>
+      </div>
+      <div>
+        <Link href="/methodology">methodology</Link>
+      </div>
     </footer>
   );
 }
